b3075763af0b649a9ec6061027a780446332565c
Monja <monja.nya@gmail.com>
2020-02-24 22:22:03 +0200

    v2.24 - JS Code Refactoring

--- script/script.js
+++ script/script.js
@@ -9,86 +9,97 @@ const tomatoUrl = 'https://i.imgur.com/KerYML6.png';
 const layers = document.querySelector('.layers');
 const priceTable = document.querySelector('.price-table-div');
 
-const morePattiesButton = document.getElementById('morePattiesButton');
-const lessPattiesButton = document.getElementById('lessPattiesButton');
-
-const moreCheeseButton = document.getElementById('moreCheeseButton');
-const lessCheeseButton = document.getElementById('lessCheeseButton');
-
-const moreLettuceButton = document.getElementById('moreLettuceButton');
-const lessLettuceButton = document.getElementById('lessLettuceButton');
-
-const moreOnionButton = document.getElementById('moreOnionButton');
-const lessOnionButton = document.getElementById('lessOnionButton');
-
-const moreTomatoButton = document.getElementById('moreTomatoButton');
-const lessTomatoButton = document.getElementById('lessTomatoButton');
-
-const pattyPiecePrice = 1.00;
-const cheesePiecePrice = 0.50;
-const lettucePiecePrice = 0.30;
-const onionPiecePrice = 0.30;
-const tomatoPiecePrice = 0.30;
-
-const pattiesLayer = document.getElementById('patties-layer');
-const cheeseLayer = document.getElementById('cheese-layer');
-const lettuceLayer = document.getElementById('lettuce-layer');
-const onionLayer = document.getElementById('onion-layer');
-const tomatoLayer = document.getElementById('tomato-layer');
-
-const prices = [pattyPiecePrice, cheesePiecePrice, lettucePiecePrice, onionPiecePrice, tomatoPiecePrice];
-
-let pattiesCount = 0;
-let cheeseCount = 0;
-let lettuceCount = 0;
-let onionCount = 0;
-let tomatoCount = 0;
-
 const upperBun = getLayerFromUrl(upperBunUrl, false);
 const bottomBun = getLayerFromUrl(bottomBunUrl, false);
 
-function removeLayer(layerToRemove) {
+class Ingredient {
 
-
-    ingridientUrl = layerToRemove.getElementsByTagName('img')[0].src;
-    let parentLayer;
-
-    switch (ingridientUrl) {
-        case pattyUrl:
-            pattiesCount--;
-            parentLayer = pattiesLayer;
-            if (pattiesCount == 0) disactivateButton(lessPattiesButton);
-            break;
-        case cheeseUrl:
-            cheeseCount--;
-            parentLayer = cheeseLayer;
-            if (cheeseCount == 0) disactivateButton(lessCheeseButton);
-            break;
-        case lettuceUrl:
-            lettuceCount--;
-            parentLayer = lettuceLayer;
-            if (lettuceCount == 0) disactivateButton(lessLettuceButton);
-            break;
-        case onionUrl:
-            onionCount--;
-            parentLayer = onionLayer;
-            if (onionCount == 0) disactivateButton(lessOnionButton);
-            break;
-        case tomatoUrl:
-            tomatoCount--;
-            parentLayer = tomatoLayer;
-            if (tomatoCount == 0) disactivateButton(lessTomatoButton);
-            break;
-        default:
-            parentLayer = layers;
+    constructor(ingredientUrl, ingredientClassName, price, layer, addButton, removeButton) {
+        this.ingredientUrl = ingredientUrl;
+        this.ingredientClassName = ingredientClassName;
+        this.price = price;
+        this.count = 0;
+        this.layer = layer;
+        this.addButton = addButton;
+        this.removeButton = removeButton;
     }
 
-    window.getComputedStyle(layerToRemove).opacity;
-    layerToRemove.style.opacity = '0';
+    sayHi() {
+        alert(this.ingredientClassName);
+    }
 
-    parentLayer.removeChild(layerToRemove)
+    removeIngredient() {
+        this.count--;
+        this.layer.removeChild(this.layer.firstChild);
+        if (this.count == 0) disactivateButton(this.removeButton);
+    }
+
+    buildIngredient(isDeletable) {
+        this.count++;
+        if (this.removeButton.disabled) activateButton(this.removeButton);
+
+        let ingredientDiv = getLayerFromUrl(this.ingredientUrl, isDeletable);
+        ingredientDiv.classList.add(this.ingredientClassName);
+        return ingredientDiv;
+    }
+
+    getIngredientUrl() {
+        return this.ingredientUrl;
+    }
+
+    getIngredientClassName() {
+        return this.ingredientClassName;
+    }
+
+    getPrice() {
+        return this.price;
+    }
+
+    getCount() {
+        return this.count;
+    }
+
+    getLayer() {
+        return this.layer;
+    }
+
+    getAddButton() {
+        return this.addButton;
+    }
+    
+    getRemoveButton() {
+        return this.removeButton;
+    }
+
+}
+
+const patty = new Ingredient(pattyUrl, "patty-ingredient", 1.00, document.getElementById('patties-layer'),
+    document.getElementById('morePattiesButton'), document.getElementById('lessPattiesButton'));
+
+const cheese = new Ingredient(cheeseUrl, "cheese-ingredient", 0.50, document.getElementById('cheese-layer'), 
+    document.getElementById('moreCheeseButton'), document.getElementById('lessCheeseButton'));
+
+const lettuce = new Ingredient(lettuceUrl, "lettuce-ingredient", 0.30, document.getElementById('lettuce-layer'),
+    document.getElementById('moreLettuceButton'), document.getElementById('lessLettuceButton'));
+
+const onion = new Ingredient(onionUrl, "onion-ingredient", 0.30, document.getElementById('onion-layer'),
+    document.getElementById('moreOnionButton'), document.getElementById('lessOnionButton'));
+
+const tomato = new Ingredient(tomatoUrl, "tomato-ingredient", 0.30, document.getElementById('tomato-layer'),
+    document.getElementById('moreTomatoButton'), document.getElementById('lessTomatoButton'));
+
+const ingredientClassesMap = new Map();
+ingredientClassesMap.set(patty.getIngredientClassName(), patty);
+ingredientClassesMap.set(cheese.getIngredientClassName(), cheese);
+ingredientClassesMap.set(lettuce.getIngredientClassName(), lettuce);
+ingredientClassesMap.set(onion.getIngredientClassName(), onion);
+ingredientClassesMap.set(tomato.getIngredientClassName(), tomato);
+
+const ingredients = [patty, cheese, lettuce, onion, tomato];
+
+function removeLayer(layerToRemove) {
+    ingredientClassesMap.get(layerToRemove.className).removeIngredient();
     updateTable();
-
 }
 
 function activateButton(button) {
@@ -107,19 +118,17 @@ function disactivateButton(button) {
 
 function setupPrices() {
 
-    for (var i = 0; i < prices.length; i++) {
+    for (var i = 0; i < ingredients.length; i++) {
         let row = priceTable.getElementsByTagName('tr')[i + 1];
         let cell = row.getElementsByTagName('td')[4];
-        cell.innerHTML = '$' + Number.parseFloat(prices[i]).toFixed(2);
+        cell.innerHTML = '$' + Number.parseFloat(ingredients[i].getPrice()).toFixed(2);
     }
 }
 
 function calculatePrice() {
 
     let price = 1.00;
-    let ingredients = [pattiesCount, cheeseCount, lettuceCount, onionCount, tomatoCount];
-
-    for (let i = 0; i < ingredients.length; i++) price += ingredients[i] * prices[i];
+    ingredients.forEach(ingredient => price += ingredient.getCount() * ingredient.getPrice());
 
     let rows = priceTable.getElementsByTagName('tr');
     let priceCell = rows[rows.length - 1].getElementsByTagName('td')[1];
@@ -129,10 +138,11 @@ function calculatePrice() {
 window.onload = function() {
 
     setupPrices();
-    setupStartBurger();
-    updateTable();
 
-    disactivateButton(lessOnionButton);
+    setupButtonInteractions();
+    ingredients.forEach(ingredient => disactivateButton(ingredient.getRemoveButton()));
+    
+    setupStartBurger();
 }
 
 function getLayerFromUrl(imageUrl, isDeletable) {
@@ -150,7 +160,6 @@ function getLayerFromUrl(imageUrl, isDeletable) {
     if (isDeletable) imgDiv.onclick = function(e) {
         removeLayer(this);
     };
-
     
     imgDiv.style.height = '60px';
     imgDiv.style.width = '60px';
@@ -167,102 +176,41 @@ function getLayerFromUrl(imageUrl, isDeletable) {
 }
 
 function updateTable() {
-    
-    let ingredients = [pattiesCount, cheeseCount, lettuceCount, onionCount, tomatoCount];
 
     for (var i = 0; i < ingredients.length; i++) {
+
+        let ingredientCount = ingredients[i].getCount();
+        
         let row = priceTable.getElementsByTagName('tr')[i + 1];
         let countCell = row.getElementsByTagName('td')[2];
-        countCell.innerHTML = ingredients[i] > 0 ? ingredients[i] : '-';
+        countCell.innerHTML = ingredientCount > 0 ? ingredientCount : '-';
 
         let priceCell = row.getElementsByTagName('td')[5];
-        priceCell.innerHTML = ingredients[i] > 0 ? '$' + Number.parseFloat(ingredients[i] * prices[i]).toFixed(2) : '-';
+        priceCell.innerHTML = ingredientCount > 0 ? '$' + Number.parseFloat(ingredientCount * ingredients[i].getPrice()).toFixed(2) : '-';
     }
 
     calculatePrice();
 }
 
-function insertLayer(layerToInsert, followingLayer) {
+function addIngredient(layerToAdd) {
 
-    switch (layerToInsert.getElementsByTagName('img')[0].src) {
-        case pattyUrl:
-            pattiesCount++;
-            pattiesLayer.appendChild(layerToInsert);
-            if (lessPattiesButton.disabled) activateButton(lessPattiesButton)
-            break;
-        case cheeseUrl:
-            cheeseCount++;
-            cheeseLayer.appendChild(layerToInsert);
-            if (lessCheeseButton.disabled) activateButton(lessCheeseButton);
-            break;
-        case lettuceUrl:
-            lettuceCount++;
-            lettuceLayer.appendChild(layerToInsert);
-            if (lessLettuceButton.disabled) activateButton(lessLettuceButton);
-            break;
-        case onionUrl:
-            onionCount++;
-            onionLayer.appendChild(layerToInsert);
-            if (lessOnionButton.disabled) activateButton(lessOnionButton);
-            break;
-        case tomatoUrl:
-            tomatoCount++;
-            tomatoLayer.appendChild(layerToInsert);
-            if (lessTomatoButton.disabled) activateButton(lessTomatoButton);
-            break;
-        default:
-            followingLayer.parentNode.insertBefore(layerToInsert, followingLayer.nextSibling);
-    }
-
-    window.getComputedStyle(layerToInsert).opacity;
-    layerToInsert.style.opacity = '1';
+    ingredientClassesMap.get(layerToAdd.className).getLayer().appendChild(layerToAdd);
+    window.getComputedStyle(layerToAdd).opacity;
+    layerToAdd.style.opacity = '1';
 
     updateTable();
 }
 
-morePattiesButton.onclick = function() {
-    insertLayer(getLayerFromUrl(pattyUrl, true), upperBun);
-}
-
-lessPattiesButton.onclick = function() {
-    removeLayer(pattiesLayer.firstChild);
-}
-
-moreCheeseButton.onclick = function() {
-    insertLayer(getLayerFromUrl(cheeseUrl, true), upperBun);
-}
-
-lessCheeseButton.onclick = function() {
-    removeLayer(cheeseLayer.firstChild);
-}
-
-moreLettuceButton.onclick = function() {
-    insertLayer(getLayerFromUrl(lettuceUrl, true), upperBun);
-}
-
-lessLettuceButton.onclick = function() {
-    removeLayer(lettuceLayer.firstChild);
-}
-
-moreOnionButton.onclick = function() {
-    insertLayer(getLayerFromUrl(onionUrl, true), upperBun);
-}
-
-lessOnionButton.onclick = function() {
-    removeLayer(onionLayer.firstChild);
-}
-
-moreTomatoButton.onclick = function() {
-    insertLayer(getLayerFromUrl(tomatoUrl, true), upperBun);
-}
-
-lessTomatoButton.onclick = function() {
-    removeLayer(tomatoLayer.firstChild);
+function setupButtonInteractions() {
+    ingredients.forEach(ingredient => {
+        ingredient.getAddButton().onclick = function() { addIngredient(ingredient.buildIngredient(true)); }
+        ingredient.getRemoveButton().onclick = function() { removeLayer(ingredient.getLayer().firstChild); }
+    })
 }
 
 function setupStartBurger() {
 
-    layers.insertBefore(upperBun, lettuceLayer);
+    layers.insertBefore(upperBun, lettuce.getLayer());
     window.getComputedStyle(upperBun).opacity;
     upperBun.style.opacity = '1';
 
@@ -270,9 +218,9 @@ function setupStartBurger() {
     window.getComputedStyle(bottomBun).opacity;
     bottomBun.style.opacity = '1';
 
-    insertLayer(getLayerFromUrl(pattyUrl, true), upperBun);
-    insertLayer(getLayerFromUrl(cheeseUrl, true), upperBun);
-    insertLayer(getLayerFromUrl(tomatoUrl, true), upperBun);
-    insertLayer(getLayerFromUrl(cheeseUrl, true), upperBun);
-    insertLayer(getLayerFromUrl(lettuceUrl, true), upperBun);
+    addIngredient(patty.buildIngredient(true));
+    addIngredient(cheese.buildIngredient(true));
+    addIngredient(tomato.buildIngredient(true));
+    addIngredient(cheese.buildIngredient(true));
+    addIngredient(lettuce.buildIngredient(true));
 }
